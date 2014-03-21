02e983f842a0222d162671ad743e96626d2cc4f1
Ryan Boone <falldowngoboone@gmail.com>
2014-03-20 20:08:20 -0500

    First commit.

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tina Boone | Young Adult Writer</title>
+</head>
+<body>
+    <header>
+        <h1 class="logo">Tina Boone</h1>
+        <nav class="nav" role="navigation">
+            <ul class="nav-list">
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="http://www.lifescripted.com/">Blog</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="about" id="about">
+        <h1 class="section-h1">About Me</h1>
+        <p class="intro">My full name is Christine Ramirez Boone, but everyone calls me Tina. I write for children, young adults and adults who sing along with aging hits that embarrass their kids in elevators and grocery stores.</p>
+        <p>Growing up, our small corner of the world had its perks like a Dairy Queen every thirty miles and trees as far as the eye could see. I was born and raised in Southeast Texas where front porches were equipped with giant wooden swings made for sipping sweet tea on hot summer days, or most days if you’re familiar with the weather. We basically had three choices of climate: hot, mild or rainy. The latter came with an added bonus: hair curling humidity and monstrous mosquitoes.</p>
+        <p>My parents moved around for several years before finally settling in Nederland, home of the Bulldogs, where I spent many hours singing Janet Jackson songs into my hairbrush, choreographing dance routines to nineties pop in my bedroom mirror, crushing on various soccer players and musicians at school and occasionally writing lame “Dear Diary” entries about those experiences.</p>
+        <p>I was one of those reluctant readers who’d rather be in the dance studio than sit still for long periods of time, but on a off day I’d catch myself writing poetry or secretly loving my English homework. At the time I was also in drill team, the Westernaires, doing what I loved most—dancing. Senior year I met my high school sweetheart (now my husband) in choir and spent the next seven months hounding my boss for time off so I could go on actual dates (I’d really had none up to that point, sad, I know).</p>
+        <p>Later, in the real world with my Bachelor’s degree stuffed between appliance manuals in a filing cabinet, I found myself scribbling poetry and fiction at the kitchen table to escape the doldrums of my first post-college job. Seeing an opportunity, I took a two-year writing course and eventually landed in a writing career.</p>
+        <p>Today, I spend most of my time working in a makeshift living room office on my young adult novel. For me, life’s most normal when I’m dancing around the house like I’m in a musical, quoting lines from movies and TV shows with my husband, watching Pillow Talk for the millionth time, or singing random songs on road trips while doing what I call “car choreography”—I keep telling my husband it’s not weird to do repetitive isolated dance moves in place, even if other drivers are staring. He’s not convinced, yet.</p>
+    </section>
+    <section class="projects" id="projects">
+        <h1 class="section-h1">My Projects</h1>
+    </section>
+    <section class="contact" id="contact">
+        <h1 class="section-h1">Contact Me</h1>
+    </section>
+</body>
+</html>
