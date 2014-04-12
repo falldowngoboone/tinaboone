87278504adf5da6b6747354b3ecdb6164b2b7c95
Ryan Boone <falldowngoboone@gmail.com>
2014-04-12 11:41:30 -0500

    Importing all Sass into one single compressed file now.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Tina Boone | Young Adult Writer</title>
+    <link rel="stylesheet" href="css/global.css">
 </head>
 <body>
     <header>
@@ -16,20 +17,44 @@
             </ul>
         </nav>
     </header>
-    <section class="about" id="about">
-        <h1 class="section-h1">About Me</h1>
-        <p class="intro">My full name is Christine Ramirez Boone, but everyone calls me Tina. I write for children, young adults and adults who sing along with aging hits that embarrass their kids in elevators and grocery stores.</p>
-        <p>Growing up, our small corner of the world had its perks like a Dairy Queen every thirty miles and trees as far as the eye could see. I was born and raised in Southeast Texas where front porches were equipped with giant wooden swings made for sipping sweet tea on hot summer days, or most days if you’re familiar with the weather. We basically had three choices of climate: hot, mild or rainy. The latter came with an added bonus: hair curling humidity and monstrous mosquitoes.</p>
-        <p>My parents moved around for several years before finally settling in Nederland, home of the Bulldogs, where I spent many hours singing Janet Jackson songs into my hairbrush, choreographing dance routines to nineties pop in my bedroom mirror, crushing on various soccer players and musicians at school and occasionally writing lame “Dear Diary” entries about those experiences.</p>
-        <p>I was one of those reluctant readers who’d rather be in the dance studio than sit still for long periods of time, but on a off day I’d catch myself writing poetry or secretly loving my English homework. At the time I was also in drill team, the Westernaires, doing what I loved most—dancing. Senior year I met my high school sweetheart (now my husband) in choir and spent the next seven months hounding my boss for time off so I could go on actual dates (I’d really had none up to that point, sad, I know).</p>
-        <p>Later, in the real world with my Bachelor’s degree stuffed between appliance manuals in a filing cabinet, I found myself scribbling poetry and fiction at the kitchen table to escape the doldrums of my first post-college job. Seeing an opportunity, I took a two-year writing course and eventually landed in a writing career.</p>
-        <p>Today, I spend most of my time working in a makeshift living room office on my young adult novel. For me, life’s most normal when I’m dancing around the house like I’m in a musical, quoting lines from movies and TV shows with my husband, watching Pillow Talk for the millionth time, or singing random songs on road trips while doing what I call “car choreography”—I keep telling my husband it’s not weird to do repetitive isolated dance moves in place, even if other drivers are staring. He’s not convinced, yet.</p>
-    </section>
-    <section class="projects" id="projects">
-        <h1 class="section-h1">My Projects</h1>
-    </section>
-    <section class="contact" id="contact">
-        <h1 class="section-h1">Contact Me</h1>
-    </section>
+    <div class="grid-container">
+        <section class="about" id="about">
+            <h1 class="section-h1">About Me</h1>
+            <p class="intro">My full name is Christine Ramirez Boone, but everyone calls me Tina. I write for children, young adults and adults who sing along with aging hits that embarrass their kids in elevators and grocery stores.</p>
+            <p>Growing up, our small corner of the world had its perks like a Dairy Queen every thirty miles and trees as far as the eye could see. I was born and raised in Southeast Texas where front porches were equipped with giant wooden swings made for sipping sweet tea on hot summer days, or most days if you’re familiar with the weather. We basically had three choices of climate: hot, mild or rainy. The latter came with an added bonus: hair curling humidity and monstrous mosquitoes.</p>
+            <p>My parents moved around for several years before finally settling in Nederland, home of the Bulldogs, where I spent many hours singing Janet Jackson songs into my hairbrush, choreographing dance routines to nineties pop in my bedroom mirror, crushing on various soccer players and musicians at school and occasionally writing lame “Dear Diary” entries about those experiences.</p>
+            <p>I was one of those reluctant readers who’d rather be in the dance studio than sit still for long periods of time, but on a off day I’d catch myself writing poetry or secretly loving my English homework. At the time I was also in drill team, the Westernaires, doing what I loved most&mdash;dancing. Senior year I met my high school sweetheart (now my husband) in choir and spent the next seven months hounding my boss for time off so I could go on actual dates (I’d really had none up to that point, sad, I know).</p>
+            <p>Later, in the real world with my Bachelor’s degree stuffed between appliance manuals in a filing cabinet, I found myself scribbling poetry and fiction at the kitchen table to escape the doldrums of my first post-college job. Seeing an opportunity, I took a two-year writing course and eventually landed in a writing career.</p>
+            <p>Today, I spend most of my time working in a makeshift living room office on my young adult novel. For me, life’s most normal when I’m dancing around the house like I’m in a musical, quoting lines from movies and TV shows with my husband, watching <cite>Pillow Talk</cite> for the millionth time, or singing random songs on road trips while doing what I call “car choreography”—I keep telling my husband it’s not weird to do repetitive isolated dance moves in place, even if other drivers are staring. He’s not convinced, yet.</p>
+        </section>
+    </div>
+    <div class="grid-container">
+        <section class="projects" id="projects">
+            <h1 class="section-h1">My Writing Projects</h1>
+            <h2 class="section-h2">Young Adult Novels</h2>
+            <dl>
+                <dt>PPKK</dt>
+                <dd>As a young dancer prepares for an important audition her perseverance is put to the test.</dd>
+                <dt>The Truth and Other Gray Areas</dt>
+                <dd>During her parents' separation, 17-year-old Kristen navigates her way out of emotional darkness.</dd>
+            </dl>
+            <h2 class="section-h2">Picture Books</h2>
+            <dl>
+                <dt>The Very Sneezy Dragon</dt>
+                <dd>Dragon must get to the doctor’s office before his allergies get the best of him, and everyone else.</dd>
+                <dt>Tooth Fairy Tooth Troubles</dt>
+                <dd>Tillie the Tooth Fairy faces a quirky conundrum when she loses one of her own teeth.</dd>
+                <dt>To Capture the Sun</dt>
+                <dd>Carmelo seeks to give his grandfather something special and discovers a new way to share what’s in his heart.</dd>
+            </dl>
+
+        </section>
+    </div>
+    <div class="grid-container">
+        <section class="contact" id="contact">
+            <h1 class="section-h1">Contact Me</h1>
+            <p>Find updates about current TV obsessions, where I am in the book writing process and other random interests on my Twitter and Facebook accounts and blog. You may also use the contact form below for questions or comments.</p>
+        </section>
+    </div>
 </body>
 </html>
