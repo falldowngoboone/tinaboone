0a8682439994bc19771d0a2e4c2c7721e90e0426
Ryan Boone <falldowngoboone@gmail.com>
2014-04-19 16:29:50 -0500

    Added Style Guide.

---
+++ styleguide.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>tinaboone.com Style Guide</title>
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/styleguide.css">
+    <link rel="stylesheet" href="css/X-rayHTML.css">
+    <link rel="stylesheet" href="themes/prism.css">
+</head>
+<body>
+    <h1>tinaboone.com Style Guide</h1>
+    <h2>Colors</h2>
+    <div>
+        <ul class="sg-colors">
+            <li class="sg-color">
+                <span class="sg-swatch" style="background:#ccc;"></span>
+                <span class="sg-label">#ccc</span>
+            </li>
+            <li class="sg-color">
+                <span class="sg-swatch" style="background:#ccc;"></span>
+                <span class="sg-label">#ccc</span>
+            </li>
+        </ul>
+    </div>
+    <h2>Fonts</h2>
+    <div></div>
+    <h2>Animations</h2>
+    <div></div>
+    <h2>Visibility</h2>
+    <div></div>
+    <h2>Headings</h2>
+    <div></div>
+    <h2>Paragraph</h2>
+    <div></div>
+    <h2>Blockquote</h2>
+    <div></div>
+    <h2>Inline Elements</h2>
+    <div></div>
+    <h2>Time</h2>
+    <div></div>
+    <h2>Preformatted Text</h2>
+    <div></div>
+    <h2>Hr</h2>
+    <div></div>
+    <h2>Unordered</h2>
+    <div></div>
+    <h2>Ordered</h2>
+    <div></div>
+    <h2>Definition</h2>
+    <div></div>
+    <h2>Logo</h2>
+    <div></div>
+    <h2>Landscape 4x3</h2>
+    <div></div>
+    <h2>Landscape 16x9</h2>
+    <div></div>
+    <h2>Square</h2>
+    <div></div>
+    <h2>Avatar</h2>
+    <div></div>
+    <h2>Icons</h2>
+    <div></div>
+    <h2>Loading Icon</h2>
+    <div></div>
+    <h2>Favicon</h2>
+    <div></div>
+    <h2>Text Fields</h2>
+    <div></div>
+    <h2>Select Menu</h2>
+    <div></div>
+    <h2>Checkbox</h2>
+    <div></div>
+    <h2>Radio Buttons</h2>
+    <div></div>
+    <h2>HTML5 Inputs</h2>
+    <div></div>
+    <h2>Buttons</h2>
+    <div></div>
+    <h2>Table</h2>
+    <div></div>
+    <h2>Byline</h2>
+    <div></div>
+    <h2>Address</h2>
+    <div></div>
+    <h2>Heading Group</h2>
+    <div></div>
+    <h2>Blockquote with Citation</h2>
+    <div></div>
+    <h2>Intro Text</h2>
+    <div></div>
+    <h2>One Up</h2>
+    <div></div>
+    <h2>Two Up</h2>
+    <div></div>
+    <h2>Three Up</h2>
+    <div></div>
+    <h2>Four Up</h2>
+    <div></div>
+    <h2>Media Block</h2>
+    <div></div>
+    <h2>Block Headline Byline</h2>
+    <div></div>
+    <h2>Block Hero</h2>
+    <div></div>
+    <h2>Block Hero Thumbnail</h2>
+    <div></div>
+    <h2>Block Headline</h2>
+    <div></div>
+    <h2>Block Inset</h2>
+    <div></div>
+    <h2>Figure with Caption</h2>
+    <div></div>
+    <h2>Search</h2>
+    <div></div>
+    <h2>Comment Form</h2>
+    <div></div>
+    <h2>Newsletter</h2>
+    <div></div>
+    <h2>Primary Nav</h2>
+    <div></div>
+    <h2>Footer Nav</h2>
+    <div></div>
+    <h2>Breadcrumbs</h2>
+    <div></div>
+    <h2>Pagination</h2>
+    <div></div>
+    <h2>Tabs</h2>
+    <div></div>
+    <h2>Social Share</h2>
+    <div></div>
+    <h2>Accordian</h2>
+    <div></div>
+    <h2>Single Comment</h2>
+    <div></div>
+    <h2>Alert</h2>
+    <div></div>
+    <h2>Header</h2>
+    <div></div>
+    <h2>Footer</h2>
+    <div></div>
+    <h2>Article Body</h2>
+    <div></div>
+    <h2>Comment Thread</h2>
+    <div></div>
+    <h2>Latest Posts</h2>
+    <div></div>
+    <h2>Recent Tweets</h2>
+    <div></div>
+    <h2>Recent Posts</h2>
+    <div></div>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/X-rayHTML.js"></script>
+    <script src="js/prism.js"></script>
+</body>
+</html>
