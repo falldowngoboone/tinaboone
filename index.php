d154af3e5157f636c936cf0521f999ba611df83d
Ryan Boone <falldowngoboone@gmail.com>
2014-07-27 16:49:45 -0500

    Deleted several files associated with creating a style guide. Added several design implementations from the Pattern Lab build.

--- index.php
+++ index.php
@@ -1,66 +1,254 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
     <title>Tina Boone | Young Adult Writer</title>
-    <link rel="stylesheet" href="css/global.css">
+    <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width" />
+        <link rel="stylesheet" href="css/style.css" media="all" />
+        <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic|Maiden+Orange' rel='stylesheet' type='text/css'>
 </head>
 <body>
-    <header>
-        <h1 class="logo">Tina Boone</h1>
-        <nav class="nav" role="navigation">
+
+<!-- Begin .header -->
+<header class="header" role="banner">
+    <span data-picture data-alt="Tina Boone">
+        <span data-src="/images/header-banner-sm.jpg"></span>
+        <span data-src="/images/header-banner.jpg" data-media="(min-width: 39.8em)"></span>
+        <!-- Fallback content for non-JS browsers. Same img src as the initial, unqualified source element. -->
+        <noscript>
+            <img src="/images/header-banner-sm.jpg" alt="Tina Boone">
+        </noscript>
+    </span>
+
+    <h1><?php include 'images/svg/logo.svg'; ?></h1>
+    <div >
+        <h2 id="logo-subtitle" class="logo-subtitle">Writer. Wife. Dance fanatic.</h2>
+    </div>
+    <nav class="nav nav-primary" role="navigation">
+        <ul class="nav-list">
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="http://www.lifescripted.com">Blog</a></li>
+        </ul>
+    </nav>
+</header>
+<!-- End .header -->
+
+<div class="intro-block">
+    <div class="lc">
+        <p class="intro">My full name is Christine Ramirez Boone, but everyone calls me Tina. I write for children, young adults and adults who sing along with aging hits to embarrass their kids in elevators and grocery stores.</p>
+    </div>
+</div>
+    <nav class="nav nav-social" role="navigation">
+        <ul class="nav-list">
+            <li><a href="https://twitter.com/cr_boone"><?php include 'images/svg/twitter.svg'; ?></a></li>
+            <li><a href="https://www.facebook.com/tinaboonewriter"><?php include 'images/svg/facebook.svg'; ?></a></li>
+            <li><a href="http://www.pinterest.com/boone1435/"><?php include 'images/svg/pinterest.svg'; ?></a></li>
+        </ul>
+    </nav>
+
+<a name="about"></a>
+<section class="about">
+
+    <header class="panel">
+        <h1 class="panel__title">About</h1>
+    </header>
+
+    <nav class="nav nav-primary" role="navigation">
+        <ul class="nav-list">
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="http://www.lifescripted.com">Blog</a></li>
+        </ul>
+    </nav>
+
+    <div class="lc">
+        <div class="l-block">
+            <div class="grid a-3">
+                <p><span class="drop-cap">G</span>rowing up, our small corner of the world had its perks like a Dairy Queen every thirty miles and trees as far as the eye could see. I was born and raised in Southeast Texas where front porches were equipped with giant wooden swings made for sipping sweet tea on hot summer days, or most days if you’re familiar with the weather. We basically had three choices of climate: hot, mild or rainy. The latter came with an added bonus: hair curling humidity and monstrous mosquitoes.</p>
+            </div>
+        </div>
+        <div class="l-block">
+            <div class="grid a-1">
+                <figure>
+                    <img src="images/about-mom.jpg" alt="Mom and me">
+                    <figcaption>Mom and me</figcaption>
+                </figure>
+            </div>
+            <div class="grid a-2">
+                <p>My parents moved around for several years before finally settling in Nederland, home of the Bulldogs, where I spent many hours singing Janet Jackson songs into my hairbrush, choreographing dance routines to nineties pop in my bedroom mirror, crushing on various soccer players and musicians at school and occasionally writing lame “Dear Diary” entries about those experiences.</p>
+                <p>I was one of those reluctant readers who’d rather be in the dance studio than sit still for long periods of time, but on an off day I’d catch myself writing poetry or secretly loving my English homework. At the time I was also in drill team, the Westernaires, doing what I loved most&mdash;dancing. Senior year I met my high school sweetheart (now my husband) in choir and spent the next seven months hounding my boss for time off so I could go on actual dates (I’d really had none up to that point, sad, I know).</p>
+            </div>
+            <div class="grid a-1">
+                <figure>
+                    <img src="images/about-dad.jpg" alt="Dad and me">
+                    <figcaption>Dad and me</figcaption>
+                </figure>
+            </div>
+        </div>
+        <div class="l-block">
+            <div class="grid a-2">
+                <p>Later, in the real world with my Bachelor’s degree stuffed between appliance manuals in a filing cabinet, I found myself scribbling poetry and fiction at the kitchen table to escape the doldrums of my first post-college job. Seeing an opportunity, I took a two-year writing course and eventually landed in a writing career.</p>
+                <p>Today, I spend most of my time working in a makeshift living room office on my young adult novel. For me, life’s most normal when I’m dancing around the house like I’m in a musical, quoting lines from movies and TV shows with my husband, watching <cite>Pillow Talk</cite> for the millionth time, or singing random songs on road trips while doing what I call “car choreography”—I keep telling my husband it’s not weird to do repetitive isolated dance moves in place, even if other drivers are staring. He’s not convinced, yet.</p>
+            </div>
+            <div class="grid a-1">
+                <figure>
+                    <img src="images/about-ballerina.jpg" alt="Future dance fanatic">
+                    <figcaption>Future dance fanatic</figcaption>
+                </figure>
+            </div>
+        </div>
+
+        <footer>
+            <nav class="nav nav-social" role="navigation">
+                <ul class="nav-list">
+                    <li><a href="https://twitter.com/cr_boone"><?php include 'images/svg/twitter.svg'; ?></a></li>
+                    <li><a href="https://www.facebook.com/tinaboonewriter"><?php include 'images/svg/facebook.svg'; ?></a></li>
+                    <li><a href="http://www.pinterest.com/boone1435/"><?php include 'images/svg/pinterest.svg'; ?></a></li>
+                </ul>
+            </nav>
+
+            <p><a class="button-top" href="#">back to the top</a></p>
+        </footer>
+    </div>
+
+</section>
+
+<a name="projects"></a>
+<section class="projects">
+
+    <header class="panel">
+        <h1 class="panel__title">Projects</h1>
+    </header>
+
+    <nav class="nav nav-primary" role="navigation">
+        <ul class="nav-list">
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="http://www.lifescripted.com">Blog</a></li>
+        </ul>
+    </nav>
+    <div class="lc">
+        <h2 class="section-h2">Young Adult Novels</h2>
+        <dl>
+            <dt>House of Dolls</dt>
+            <dd>When another girl goes missing in Savanna’s small urban town, she must face past and present fears to find the kidnapper before he finds her.</dd>
+            <dt>Point. Point. Kiss. Kiss.</dt>
+            <dd>After a descent into the social toilet, Gabby quicksteps to keep her family from falling apart and her dance team dreams alive.</dd>
+            <dt>The Truth and Other Gray Areas</dt>
+            <dd>During her parents' separation, 17-year-old Kristen navigates her way out of emotional darkness.</dd>
+        </dl>
+        <h2 class="section-h2">Picture Books</h2>
+        <dl>
+            <dt>The Very Sneezy Dragon</dt>
+            <dd>Dragon must get to the doctor’s office before his allergies get the best of him, and everyone else.</dd>
+            <dt>Tooth Fairy Tooth Troubles</dt>
+            <dd>Tillie the Tooth Fairy faces a quirky conundrum when she loses one of her own teeth.</dd>
+            <dt>To Capture the Sun</dt>
+            <dd>Carmelo seeks to give his grandfather something special and discovers a new way to share what’s in his heart.</dd>
+        </dl>
+
+        <footer>
+            <nav class="nav nav-social" role="navigation">
+                <ul class="nav-list">
+                    <li><a href="https://twitter.com/cr_boone"><?php include 'images/svg/twitter.svg'; ?></a></li>
+                    <li><a href="https://www.facebook.com/tinaboonewriter"><?php include 'images/svg/facebook.svg'; ?></a></li>
+                    <li><a href="http://www.pinterest.com/boone1435/"><?php include 'images/svg/pinterest.svg'; ?></a></li>
+                </ul>
+            </nav>
+            <p><a class="button-top" href="#">back to the top</a></p>
+        </footer>
+    </div>
+</section>
+
+<a name="contact"></a>
+<section class="contact">
+    <header class="panel">
+        <h1 class="panel__title">Contact</h1>
+    </header>
+    <nav class="nav nav-primary" role="navigation">
+        <ul class="nav-list">
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="http://www.lifescripted.com">Blog</a></li>
+        </ul>
+    </nav>
+    <div class="lc">
+        <p>Find updates about current TV obsessions, where I am in the book writing process and other random interests on my Twitter and Facebook accounts and blog. You may also use the contact form below for questions or comments.</p>
+        <form method="post" action="#" class="contact-form">
+            <fieldset>
+                <div class="field-container">
+                    <label for="comment-name">Name</label>
+                    <input id="comment-name" type="text" placeholder="Enter Your Name" />
+                </div>
+                <div class="field-container">
+                    <label for="comment-email">Email</label>
+                    <input id="comment-email" type="email" placeholder="Your email address" />
+                </div>
+                <div class="field-container">
+                    <label for="comment-field">Message</label>
+                    <textarea id="comment-field" rows="5" placeholder="Write your message here"></textarea>
+                </div>
+                <p><a href="#" class="btn">Button</a></p>
+            </fieldset>
+        </form>
+        <footer>
+            <nav class="nav nav-social" role="navigation">
+                <ul class="nav-list">
+                    <li><a href="https://twitter.com/cr_boone"><?php include 'images/svg/twitter.svg'; ?></a></li>
+                    <li><a href="https://www.facebook.com/tinaboonewriter"><?php include 'images/svg/facebook.svg'; ?></a></li>
+                    <li><a href="http://www.pinterest.com/boone1435/"><?php include 'images/svg/pinterest.svg'; ?></a></li>
+                </ul>
+            </nav>
+
+            <p><a class="button-top" href="#">back to the top</a></p>
+        </footer>
+    </div>
+</section>
+
+<!-- Begin Footer -->
+<footer class="footer" role="contentinfo">
+    <div class="lc">
+        <nav class="nav nav-primary" role="navigation">
             <ul class="nav-list">
                 <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
-                <li><a href="http://www.lifescripted.com/">Blog</a></li>
+                <li><a href="http://www.lifescripted.com">Blog</a></li>
             </ul>
         </nav>
-    </header>
-    <div class="grid-container">
-        <section class="about" id="about">
-            <h1 class="section-h1">About Me</h1>
-            <p class="intro">My full name is Christine Ramirez Boone, but everyone calls me Tina. I write for children, young adults and adults who sing along with aging hits that embarrass their kids in elevators and grocery stores.</p>
-            <p>Growing up, our small corner of the world had its perks like a Dairy Queen every thirty miles and trees as far as the eye could see. I was born and raised in Southeast Texas where front porches were equipped with giant wooden swings made for sipping sweet tea on hot summer days, or most days if you’re familiar with the weather. We basically had three choices of climate: hot, mild or rainy. The latter came with an added bonus: hair curling humidity and monstrous mosquitoes.</p>
-            <p>My parents moved around for several years before finally settling in Nederland, home of the Bulldogs, where I spent many hours singing Janet Jackson songs into my hairbrush, choreographing dance routines to nineties pop in my bedroom mirror, crushing on various soccer players and musicians at school and occasionally writing lame “Dear Diary” entries about those experiences.</p>
-            <p>I was one of those reluctant readers who’d rather be in the dance studio than sit still for long periods of time, but on a off day I’d catch myself writing poetry or secretly loving my English homework. At the time I was also in drill team, the Westernaires, doing what I loved most&mdash;dancing. Senior year I met my high school sweetheart (now my husband) in choir and spent the next seven months hounding my boss for time off so I could go on actual dates (I’d really had none up to that point, sad, I know).</p>
-            <p>Later, in the real world with my Bachelor’s degree stuffed between appliance manuals in a filing cabinet, I found myself scribbling poetry and fiction at the kitchen table to escape the doldrums of my first post-college job. Seeing an opportunity, I took a two-year writing course and eventually landed in a writing career.</p>
-            <p>Today, I spend most of my time working in a makeshift living room office on my young adult novel. For me, life’s most normal when I’m dancing around the house like I’m in a musical, quoting lines from movies and TV shows with my husband, watching <cite>Pillow Talk</cite> for the millionth time, or singing random songs on road trips while doing what I call “car choreography”—I keep telling my husband it’s not weird to do repetitive isolated dance moves in place, even if other drivers are staring. He’s not convinced, yet.</p>
+    </div>
+    <div class="lc">
+        <section class="section related-posts">
+            <h3 class="section-title">Recent Tweets</h3>
+            <ul class="headline-list">
+                <li><h4><a href="#">Headline Lorem ipsum dolor sit amet</a></h4> <time datetime="2013-04-06T12:32+00:00">2 weeks ago</time></li>
+                <li><h4><a href="#">Laborum, molestiae, rerum, dolores eveniet </a></h4> <time datetime="2013-04-06T12:32+00:00">2 weeks ago</time></li>
+                <li><h4><a href="#">quae est fugit distinctio iure odit minus non ipsum </a></h4> <time datetime="2013-04-06T12:32+00:00">2 weeks ago</time></li>
+                <li><h4><a href="#">dolorum deserunt omnis voluptates voluptatum</a></h4> <time datetime="2013-04-06T12:32+00:00">2 weeks ago</time></li>
+                <li><h4><a href="#">nventore dignissimos vel molestiae, rerum, dolore</a></h4> <time datetime="2013-04-06T12:32+00:00">2 weeks ago</time></li>
+                <li><h4><a href="#"></a></h4></li>
+            </ul>
+            <a href="#" class="text-btn">Follow us on Twitter</a>
         </section>
     </div>
-    <div class="grid-container">
-        <section class="projects" id="projects">
-            <h1 class="section-h1">My Writing Projects</h1>
-            <h2 class="section-h2">Young Adult Novels</h2>
-            <dl>
-                <dt>PPKK</dt>
-                <dd>As a young dancer prepares for an important audition her perseverance is put to the test.</dd>
-                <dt>The Truth and Other Gray Areas</dt>
-                <dd>During her parents' separation, 17-year-old Kristen navigates her way out of emotional darkness.</dd>
-            </dl>
-            <h2 class="section-h2">Picture Books</h2>
-            <dl>
-                <dt>The Very Sneezy Dragon</dt>
-                <dd>Dragon must get to the doctor’s office before his allergies get the best of him, and everyone else.</dd>
-                <dt>Tooth Fairy Tooth Troubles</dt>
-                <dd>Tillie the Tooth Fairy faces a quirky conundrum when she loses one of her own teeth.</dd>
-                <dt>To Capture the Sun</dt>
-                <dd>Carmelo seeks to give his grandfather something special and discovers a new way to share what’s in his heart.</dd>
-            </dl>
+    <div class="lc">
+        <p class="copyright">&copy; 2014 Tina Boone. All rights reserved.</p>
+    </div>
+</footer>
+<!-- End Footer -->
 
-        </section>
-    </div>
-    <div class="grid-container">
-        <section class="contact" id="contact">
-            <h1 class="section-h1">Contact Me</h1>
-            <p>Find updates about current TV obsessions, where I am in the book writing process and other random interests on my Twitter and Facebook accounts and blog. You may also use the contact form below for questions or comments.</p>
-        </section>
-    </div>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="js/jquery.window-box.js"></script>
+    <script src="/js/picturefill.js"></script>
+    <script src="/js/jquery.fittext.js"></script>
     <script>
 
-        $(document).ready(windowBox());
+        $('#logo-subtitle').fitText(1.25);
+        $('.panel__title').fitText(1, { minFontSize: '40px'});
 
     </script>
 </body>
