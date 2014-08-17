7d25146a8f338fc9445cee2dbaa581d3384e1866
Ryan Boone <falldowngoboone@gmail.com>
2014-08-17 17:39:08 -0500

    Base website configuration. This is the first working master of 2.0

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,11 +7,12 @@
         <meta name="viewport" content="width=device-width" />
         <link rel="stylesheet" href="css/style.css" media="all" />
         <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic|Maiden+Orange' rel='stylesheet' type='text/css'>
+        <script src="js/modernizr.js"></script>
 </head>
 <body>
 
 <!-- Begin .header -->
-<header class="header" role="banner">
+<header class="header" id="header" role="banner">
     <span data-picture data-alt="Tina Boone">
         <span data-src="/images/header-banner-sm.jpg"></span>
         <span data-src="/images/header-banner.jpg" data-media="(min-width: 39.8em)"></span>
@@ -66,22 +68,23 @@
 
     <div class="lc">
         <div class="l-block">
-            <div class="grid a-3">
-                <p><span class="drop-cap">G</span>rowing up, our small corner of the world had its perks like a Dairy Queen every thirty miles and trees as far as the eye could see. I was born and raised in Southeast Texas where front porches were equipped with giant wooden swings made for sipping sweet tea on hot summer days, or most days if you’re familiar with the weather. We basically had three choices of climate: hot, mild or rainy. The latter came with an added bonus: hair curling humidity and monstrous mosquitoes.</p>
+            <div class="l-main">
+                <p><span class="drop-cap">G</span>rowing up, our small corner of the world had its perks like a Dairy Queen every thirty miles and trees as far as the eye could see. I was born and raised in Southeast Texas where front porches were equipped with giant wooden swings made for sipping sweet tea on hot summer days, or most days if you&rsquo;re familiar with the weather. We basically had three choices of climate: hot, mild or rainy. The latter came with an added bonus: hair curling humidity and monstrous mosquitoes.</p>
+                <p>My parents moved around for several years before finally settling in Nederland, home of the Bulldogs, where I spent many hours singing Janet Jackson songs into my hairbrush, choreographing dance routines to nineties pop in my bedroom mirror, crushing on various soccer players and musicians at school and occasionally writing lame &ldquo;Dear Diary&rdquo; entries about those experiences.</p>
             </div>
-        </div>
-        <div class="l-block">
-            <div class="grid a-1">
+            <div class="l-enrich">
                 <figure>
                     <img src="images/about-mom.jpg" alt="Mom and me">
                     <figcaption>Mom and me</figcaption>
                 </figure>
             </div>
-            <div class="grid a-2">
-                <p>My parents moved around for several years before finally settling in Nederland, home of the Bulldogs, where I spent many hours singing Janet Jackson songs into my hairbrush, choreographing dance routines to nineties pop in my bedroom mirror, crushing on various soccer players and musicians at school and occasionally writing lame “Dear Diary” entries about those experiences.</p>
-                <p>I was one of those reluctant readers who’d rather be in the dance studio than sit still for long periods of time, but on an off day I’d catch myself writing poetry or secretly loving my English homework. At the time I was also in drill team, the Westernaires, doing what I loved most&mdash;dancing. Senior year I met my high school sweetheart (now my husband) in choir and spent the next seven months hounding my boss for time off so I could go on actual dates (I’d really had none up to that point, sad, I know).</p>
+        </div>
+        <div class="l-block l-reverse">
+            <div class="l-main">
+                <p>I was one of those reluctant readers who&rsquo;d rather be in the dance studio than sit still for long periods of time, but on an off day I&rsquo;d catch myself writing poetry or secretly loving my English homework. At the time I was also in drill team, the Westernaires, doing what I loved most&mdash;dancing. Senior year I met my high school sweetheart (now my husband) in choir and spent the next seven months hounding my boss for time off so I could go on actual dates (I&rsquo;d really had none up to that point, sad, I know).</p>
+                <p>Later, in the real world with my Bachelor&rsquo;s degree stuffed between appliance manuals in a filing cabinet, I found myself scribbling poetry and fiction at the kitchen table to escape the doldrums of my first post-college job. Seeing an opportunity, I took a two-year writing course and eventually landed in a writing career.</p>
             </div>
-            <div class="grid a-1">
+            <div class="l-enrich">
                 <figure>
                     <img src="images/about-dad.jpg" alt="Dad and me">
                     <figcaption>Dad and me</figcaption>
@@ -89,11 +92,10 @@
             </div>
         </div>
         <div class="l-block">
-            <div class="grid a-2">
-                <p>Later, in the real world with my Bachelor’s degree stuffed between appliance manuals in a filing cabinet, I found myself scribbling poetry and fiction at the kitchen table to escape the doldrums of my first post-college job. Seeing an opportunity, I took a two-year writing course and eventually landed in a writing career.</p>
-                <p>Today, I spend most of my time working in a makeshift living room office on my young adult novel. For me, life’s most normal when I’m dancing around the house like I’m in a musical, quoting lines from movies and TV shows with my husband, watching <cite>Pillow Talk</cite> for the millionth time, or singing random songs on road trips while doing what I call “car choreography”—I keep telling my husband it’s not weird to do repetitive isolated dance moves in place, even if other drivers are staring. He’s not convinced, yet.</p>
+            <div class="l-main">
+                <p>Today, I spend most of my time working in a makeshift living room office on my young adult novel. For me, life&rsquo;s most normal when I&rsquo;m dancing around the house like I&rsquo;m in a musical, quoting lines from movies and TV shows with my husband, watching <cite>Pillow Talk</cite> for the millionth time, or singing random songs on road trips while doing what I call &ldquo;car choreography&rdquo;&mdash;I keep telling my husband it&rsquo;s not weird to do repetitive isolated dance moves in place, even if other drivers are staring. He&rsquo;s not convinced,&nbsp;yet.</p>
             </div>
-            <div class="grid a-1">
+            <div class="l-enrich">
                 <figure>
                     <img src="images/about-ballerina.jpg" alt="Future dance fanatic">
                     <figcaption>Future dance fanatic</figcaption>
@@ -102,15 +104,7 @@
         </div>
 
         <footer>
-            <nav class="nav nav-social" role="navigation">
-                <ul class="nav-list">
-                    <li><a href="https://twitter.com/cr_boone"><?php include 'images/svg/twitter.svg'; ?></a></li>
-                    <li><a href="https://www.facebook.com/tinaboonewriter"><?php include 'images/svg/facebook.svg'; ?></a></li>
-                    <li><a href="http://www.pinterest.com/boone1435/"><?php include 'images/svg/pinterest.svg'; ?></a></li>
-                </ul>
-            </nav>
-
-            <p><a class="button-top" href="#">back to the top</a></p>
+            <p><a class="btn-top" href="#header">back to the top</a></p>
         </footer>
     </div>
 
@@ -132,34 +126,41 @@
         </ul>
     </nav>
     <div class="lc">
-        <h2 class="section-h2">Young Adult Novels</h2>
-        <dl>
-            <dt>House of Dolls</dt>
-            <dd>When another girl goes missing in Savanna’s small urban town, she must face past and present fears to find the kidnapper before he finds her.</dd>
-            <dt>Point. Point. Kiss. Kiss.</dt>
-            <dd>After a descent into the social toilet, Gabby quicksteps to keep her family from falling apart and her dance team dreams alive.</dd>
-            <dt>The Truth and Other Gray Areas</dt>
-            <dd>During her parents' separation, 17-year-old Kristen navigates her way out of emotional darkness.</dd>
+
+        <h2 class="section-h2 book-group__title">Young Adult Novels</h2>
+        <dl class="book-group">
+            <div class="book-item">
+                <dt class="book-item__title">House of Dolls</dt>
+                <dd class="book-item__desc">When another girl goes missing in Savanna&rsquo;s small urban town, she must face past and present fears or she might be next on the kidnapper&rsquo;s&nbsp;list.</dd>
+            </div>
+            <div class="book-item">
+                <dt class="book-item__title">Point. Point. Kiss.&nbsp;Kiss.</dt>
+                <dd class="book-item__desc">After a descent into the social toilet, Gabby quicksteps to keep her family from falling apart and her dance team dreams&nbsp;alive.</dd>
+            </div>
+            <div class="book-item">
+                <dt class="book-item__title">The Truth and Other Gray&nbsp;Areas</dt>
+                <dd class="book-item__desc">During her parents&rsquo; separation, 17-year-old Kristen navigates her way out of emotional darkness.</dd>
+            </div>
         </dl>
-        <h2 class="section-h2">Picture Books</h2>
-        <dl>
-            <dt>The Very Sneezy Dragon</dt>
-            <dd>Dragon must get to the doctor’s office before his allergies get the best of him, and everyone else.</dd>
-            <dt>Tooth Fairy Tooth Troubles</dt>
-            <dd>Tillie the Tooth Fairy faces a quirky conundrum when she loses one of her own teeth.</dd>
-            <dt>To Capture the Sun</dt>
-            <dd>Carmelo seeks to give his grandfather something special and discovers a new way to share what’s in his heart.</dd>
+        <h2 class="section-h2 book-group__title">Picture Books</h2>
+        <dl class="book-group">
+            <div class="book-item">
+                <dt class="book-item__title">The Very Sneezy Dragon</dt>
+                <dd class="book-item__desc">Dragon must get to the doctor&rsquo;s office before his allergies get the best of him, and everyone&nbsp;else.</dd>
+            </div>
+            <div class="book-item">
+                <dt class="book-item__title">Tooth Fairy Tooth&nbsp;Troubles</dt>
+                <dd class="book-item__desc">Tillie the Tooth Fairy faces a quirky conundrum when she loses one of her own&nbsp;teeth.</dd>
+            </div>
+            <div class="book-item">
+                <dt class="book-item__title">To Capture the&nbsp;Sun</dt>
+                <dd class="book-item__desc">Carmelo seeks to give his grandfather something special and discovers a new way to share what’s in his&nbsp;heart.</dd>
+            </div>
         </dl>
 
+
         <footer>
-            <nav class="nav nav-social" role="navigation">
-                <ul class="nav-list">
-                    <li><a href="https://twitter.com/cr_boone"><?php include 'images/svg/twitter.svg'; ?></a></li>
-                    <li><a href="https://www.facebook.com/tinaboonewriter"><?php include 'images/svg/facebook.svg'; ?></a></li>
-                    <li><a href="http://www.pinterest.com/boone1435/"><?php include 'images/svg/pinterest.svg'; ?></a></li>
-                </ul>
-            </nav>
-            <p><a class="button-top" href="#">back to the top</a></p>
+            <p><a class="btn-top" href="#header">back to the top</a></p>
         </footer>
     </div>
 </section>
@@ -178,24 +179,63 @@
         </ul>
     </nav>
     <div class="lc">
-        <p>Find updates about current TV obsessions, where I am in the book writing process and other random interests on my Twitter and Facebook accounts and blog. You may also use the contact form below for questions or comments.</p>
-        <form method="post" action="#" class="contact-form">
-            <fieldset>
-                <div class="field-container">
-                    <label for="comment-name">Name</label>
-                    <input id="comment-name" type="text" placeholder="Enter Your Name" />
-                </div>
-                <div class="field-container">
-                    <label for="comment-email">Email</label>
-                    <input id="comment-email" type="email" placeholder="Your email address" />
-                </div>
-                <div class="field-container">
-                    <label for="comment-field">Message</label>
-                    <textarea id="comment-field" rows="5" placeholder="Write your message here"></textarea>
-                </div>
-                <p><a href="#" class="btn">Button</a></p>
-            </fieldset>
-        </form>
+        <div class="l-block">
+            <p>Find updates about current TV obsessions, where I am in the book writing process and other random interests on my <a href="https://twitter.com/cr_boone">Twitter</a> and <a href="https://www.facebook.com/tinaboonewriter">Facebook</a> accounts and <a href="http://www.lifescripted.com">blog</a>. You may also use the contact form below for questions or comments.</p>
+
+            <?php
+            //init variables
+            $cf = array();
+            $sr = false;
+
+            if(isset($_SESSION['cf_returndata'])){
+                $cf = $_SESSION['cf_returndata'];
+                $sr = true;
+            }
+            ?>
+
+            <ul id="errors" class="error <?php echo ($sr && !$cf['form_ok']) ? 'visible' : ''; ?>">
+                <li id="info">There were some problems with your form submission:</li>
+                <?php
+                if(isset($cf['errors']) && count($cf['errors']) > 0) :
+                    foreach($cf['errors'] as $error) :
+                ?>
+                <li><?php echo $error ?></li>
+                <?php
+                    endforeach;
+                endif;
+                ?>
+            </ul>
+            <p id="success" class="valid <?php echo ($sr && $cf['form_ok']) ? 'visible' : ''; ?>">Thanks for your message! I'll get back to you ASAP!</p>
+
+            <form method="post" action="scripts/process.php" class="contact-form">
+                <fieldset>
+                    <div class="field-container">
+                        <label for="contact-name">Name</label>
+                        <input id="contact-name" name="contact-name" type="text" value="<?php echo ($sr && !$cf['form_ok']) ? $cf['posted_form_data']['name'] : '' ?>" placeholder="John Doe" required />
+                    </div>
+                    <div class="field-container">
+                        <label for="contact-email">Email</label>
+                        <input id="contact-email" name="contact-email" type="email" value="<?php echo ($sr && !$cf['form_ok']) ? $cf['posted_form_data']['email'] : '' ?>" placeholder="johndoe@example.com" required />
+                    </div>
+                    <div class="field-container">
+                        <label for="contact-message">Message</label>
+                        <textarea id="contact-message" name="contact-message" rows="5" placeholder="Your message must be greater than 20 characters" required><?php echo ($sr && !$cf['form_ok']) ? $cf['posted_form_data']['message'] : '' ?></textarea>
+                    </div>
+                    <div class="field-container" style="display:none;">
+                        <label for="contact-address">Address</label>
+                        <input id="contact-address" name="contact-address" type="text" placeholder="If you are seeing this, leave this blank. Thanks!" />
+                    </div>
+
+                    <span id="loading">Loading...</span>
+                    <input class="btn" id="submit-button" type="submit" value="Send Message">
+
+                </fieldset>
+            </form>
+
+            <?php unset($_SESSION['cf_returndata']); ?>
+
+        </div>
+
         <footer>
             <nav class="nav nav-social" role="navigation">
                 <ul class="nav-list">
@@ -205,7 +245,7 @@
                 </ul>
             </nav>
 
-            <p><a class="button-top" href="#">back to the top</a></p>
+            <p><a class="btn-top" href="#header">back to the top</a></p>
         </footer>
     </div>
 </section>
@@ -223,17 +263,14 @@
         </nav>
     </div>
     <div class="lc">
-        <section class="section related-posts">
-            <h3 class="section-title">Recent Tweets</h3>
-            <ul class="headline-list">
-                <li><h4><a href="#">Headline Lorem ipsum dolor sit amet</a></h4> <time datetime="2013-04-06T12:32+00:00">2 weeks ago</time></li>
-                <li><h4><a href="#">Laborum, molestiae, rerum, dolores eveniet </a></h4> <time datetime="2013-04-06T12:32+00:00">2 weeks ago</time></li>
-                <li><h4><a href="#">quae est fugit distinctio iure odit minus non ipsum </a></h4> <time datetime="2013-04-06T12:32+00:00">2 weeks ago</time></li>
-                <li><h4><a href="#">dolorum deserunt omnis voluptates voluptatum</a></h4> <time datetime="2013-04-06T12:32+00:00">2 weeks ago</time></li>
-                <li><h4><a href="#">nventore dignissimos vel molestiae, rerum, dolore</a></h4> <time datetime="2013-04-06T12:32+00:00">2 weeks ago</time></li>
-                <li><h4><a href="#"></a></h4></li>
-            </ul>
-            <a href="#" class="text-btn">Follow us on Twitter</a>
+        <section class="block-twitter l-block">
+            <h3 class="section-title">Recent Tweet</h3>
+            <div id="twitter">
+                <ul>
+                    <li>Twitter feed loading</li>
+                </ul>
+            </div>
+            <p id="follow"><a href="https://twitter.com/cr_boone" target="blank">Follow me on Twitter.</a></p>
         </section>
     </div>
     <div class="lc">
@@ -243,13 +280,17 @@
 <!-- End Footer -->
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="/js/picturefill.js"></script>
+    <script src="js/picturefill.js"></script>
+    <script src="js/global.js"></script>
+    <!-- fittext.js -->
     <script src="/js/jquery.fittext.js"></script>
     <script>
 
         $('#logo-subtitle').fitText(1.25);
-        $('.panel__title').fitText(1, { minFontSize: '40px'});
+        $('.panel__title').fitText(1, { minFontSize: '60px'});
 
     </script>
+
+
 </body>
 </html>
